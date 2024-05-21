a61575985fd623afef4f20b3283424a247b581a5
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-05-21 16:52:54 +0200

    Added modal to the survey for an extra caution step

---
+++ app/Dashboard/survey/SubmitModal.tsx
@@ -0,0 +1,58 @@
+import { Button, Modal, Box, Typography } from "@mui/material";
+import React from "react";
+import { CgDanger } from "react-icons/cg";
+
+type Props = {
+  handleSubmit: (event: React.FormEvent) => void;
+};
+
+export default function BasicModal({ handleSubmit }: Props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button
+        className="bg-gradient-primary p-2.5 font-bold mt-5 mb-5 w-full"
+        color="secondary"
+        variant="outlined"
+        onClick={handleOpen}
+      >
+        Versturen
+      </Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box className="absolute top-1/4 left-2/4 -translate-x-10 -translate-y-10 w-1/4 p-10  bg-white">
+          <Box
+            component={"article"}
+            className="flex flex-col items-center justify-center gap-2"
+          >
+            <CgDanger size={"45px"} />
+            <Typography fontWeight={"bold"} fontSize={"20px"}>
+              Beschikbaarheid versturen
+            </Typography>
+            <Typography align="center" fontSize={"15px"}>
+              Weet je zeker dat je alles correct hebt ingevuld?
+            </Typography>
+            <Box
+              component={"div"}
+              className="flex justify-center items-center gap-5 mt-5"
+            >
+              <Button onClick={handleClose} color="primary" variant="outlined">
+                Terug
+              </Button>
+              <Button
+                onClick={handleSubmit}
+                color="secondary"
+                className="bg-gradient-primary text-white"
+                variant="outlined"
+              >
+                Versturen
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
