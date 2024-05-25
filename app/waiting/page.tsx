a1ee8863ca4f0d7681bb0f601721a51241f29aef
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-05-25 21:14:19 +0200

    Updating authorization by adding a waiting section if the user has "guest" role

---
+++ app/waiting/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen flex-col gap-10'>
+        <Image src={'/availi.svg'} width={200} height={200} alt='logo' />
+        <p className='text-4xl font-semibold'>Wacht even totdat de admin je toelaat...</p>
+    </div>
+  )
+}
+
+export default page
