7fccb83aaba96579312ae6be12dcae740e0943d4
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-06-04 14:33:32 +0200

    testing

--- api/auth.ts
+++ api/auth.ts
@@ -27,8 +27,7 @@ export const loginUsers = async ({
   try {
     const response = await axios.post(
       `https://availi.azurewebsites.net/api/Auth/login`,
-      body,
-      { withCredentials: true }
+      body
     );
     if (response && response.data) {
       const data = response.data;
